cd05a303387cf8d2c2a71f764d2b288140aaa843
Bryan Marlow <brylow@aevon.cloud>
2025-06-21 15:46:58 +0000

    continue

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,6 @@
 /**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * @fileOverview Main entry point for all Cloud Functions.
+ * This file exports the functions that will be deployed.
  */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 export { lemonsqueezyWebhook } from "./lemonsqueezy";
