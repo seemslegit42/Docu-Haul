fb36741beddaf8ddeb5ee315f9683dc9405a30c3
Bryan Marlow <brylow@aevon.cloud>
2025-06-22 02:14:13 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  allowedDevOrigins: ["**"],
+};
+
+module.exports = nextConfig;
